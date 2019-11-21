793d58f13fd6a592e977180976bfb39836fbe1c4
Wilder8 <jacob.wilder826@gmail.com>
2019-11-21 12:48:05 -0500

    Updates

---
+++ precache-manifest.ec200d480c65120f262d2d4a1c4bb34f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da28b503138b42c59e5bb39347eb90d7",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "401f0d4e6d11cf66ccc5",
+    "url": "/clicky-game/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "6caade2ccee642e0e622",
+    "url": "/clicky-game/static/js/2.ce40d6ae.chunk.js"
+  },
+  {
+    "revision": "401f0d4e6d11cf66ccc5",
+    "url": "/clicky-game/static/js/main.43abe7f2.chunk.js"
+  },
+  {
+    "revision": "166d09415de4c52327d7",
+    "url": "/clicky-game/static/js/runtime-main.cf0d8eec.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/clicky-game/static/media/logo.25bf045c.svg"
+  }
+]);
